e0a29aca7bba71ff27ac42d99506dbb215950bea
ryan2625 <ryanfreas19@augustana.edu>
2024-04-04 00:08:02 -0500

    Updating pattern and adding comments for regex patterns

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ directory = os.path.abspath("./String-Finder-1.0.0/Parts-X")
 # Looking through C# files that DON'T have .designer in the extension
 def find_sql_data_sources(directory):
     sql_data_sources = []
-    patterns = [r"SELECT ", r"select ", r'SqlCommand\("', r'SqlDataAdapter\("']
+    # Depending on what SQL injections we actually want to grab, edit patterns
+    # Possible patterns to include:
+    # ONLY include SELECT, select, UPDATE, DELETE, etc if it is preceeded by new SqlCommand(" ??
+    # Add DELETE, other SQL query modifiers 
+    # patterns = [r'"SELECT ', r'"select ', r'SqlCommand\("', r'SqlDataAdapter\("']
+    # patterns = [r'SqlCommand\("', r'SqlDataAdapter\("', r'SqlCommand\("', r'"UPDATE ', r'"update ', r'"SELECT ', r'"select ', r'"DELETE ', r'"delete ', r'"INSERT ', r'"insert ',]
 
+    # Patterns: Most important is where we actually declare the full command 
+    # (ex. SqlCommand\("', r'SqlDataAdapter\("', r'SqlCommand\("
+    # Do we just want that part or do we want the extra injections that can me
+    # referenced as variables such as declaring
+    # a primitive like string sql1 = "SELECT DATA from EXAMPLE"
+    
+    patterns = [r'SqlCommand\("', r'SqlDataAdapter\("', r'SqlCommand\("']
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".cs") and not file.endswith(".designer.cs"):
