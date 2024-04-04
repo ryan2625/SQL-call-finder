1e864b1652894ba05892fbb68c9d1c959b4affa4
ryan2625 <ryanfreas19@augustana.edu>
2024-04-03 21:56:54 -0500

    Bug fixing repeated entries due to multiple instances of patterns

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 
 from openpyxl import Workbook
 
-directory = os.path.abspath("../python/String-Finder-1.0.0/Parts-X")
+directory = os.path.abspath("./String-Finder-1.0.0/Parts-X")
 
 # Looking through C# files that DON'T have .designer in the extension
 def find_sql_data_sources(directory):
@@ -17,29 +17,35 @@ def find_sql_data_sources(directory):
                 with open(filepath, "r") as f:
                     content = f.readlines()
                     for line_number, line in enumerate(content, 1):
-                        for pattern in patterns:
-                            if re.search(pattern, line):
-                                # Exclude lines that contain //, we don't need to include commented lines
-                                match = re.match(r'^/', line.strip())
-                                if not match:
-                                    sql_data_sources.append(
-                                        (file, line_number, line.strip())
-                                    )
+                        if (line_number > 265):
+                            for pattern in patterns:
+                                if re.search(pattern, line):
+                                    # Trying to ? exclude lines that contain //, we don't need to include commented lines
+                                    match = re.match(r'^/', line.strip())
+                                    if not match:
+                                        sql_data_sources.append(
+                                            (file, line_number, line.strip())
+                                        )
+                                        # If multiple patterns appear on same line, we will get duplicate entries (using break to prevent this)
+                                        break
 
     return sql_data_sources
 
 
 sql_data_sources = find_sql_data_sources(directory)
-
+print(os.listdir(directory))
 # Convert to excel file etc
 wb = Workbook()
 ws = wb.active
+ws.append(["Path: IREM/Parts/.cs"])
+ws.append(["Pattern: [r'SELECT ', r'SqlCommand\("', rSqlDataAdapter\("]'])
+ws.append([""])
 ws.append(["File", "Line Number", "Select Command"])
 
 for file, line_number, command in sql_data_sources:
     ws.append([file, line_number, command])
 
-excel_file = "IREM/Parts/c#-files.xlsx"
+excel_file = "c#-files.xlsx"
 wb.save(excel_file)
 print(f"Excel file '{excel_file}' has been created.")
 
@@ -71,11 +77,14 @@ sql_data_sources2 = find_sql_data_sources2(directory)
 # Convert to excel file etc
 wb = Workbook()
 ws = wb.active
+ws.append(["Path: IREM/Parts/.ascx"])
+ws.append(["Pattern: [r'<asp:SqlDataSource']"])
+ws.append([""])
 ws.append(["File", "Line Number", "Select Command"])
 
 for file, line_number, command in sql_data_sources2:
     ws.append([file, line_number, command])
 
-excel_file = "IREM/Parts/ascx-files.xlsx"
+excel_file = "ascx-files.xlsx"
 wb.save(excel_file)
 print(f"Excel file '{excel_file}' has been created.")
