c5cabfe54fdc7c171f8ad1465bfc02c8cd343521
rfreas <rfreas@irem.org>
2024-04-04 20:58:47 -0500

    Refactoring and modularizing: functions for each extension

--- main.py
+++ main.py
@@ -1,81 +1,82 @@
 import os
 import re
-
 from openpyxl import Workbook
 
-directory = os.path.abspath("../python/String-Finder-1.0.0/Parts-X")
+entries = [
+    (
+        os.path.abspath("../python/String-Finder-1.0.0/Parts-X"),
+        ["IREM/Parts/.cs", 
+        [r'SqlDataAdapter\("', r'SqlCommand\("', r'"UPDATE', r'"update', r'"SELECT', r'"select', r'"DELETE', r'"delete', r'"INSERT', r'"insert'],
+        "IREM-Parts-c#-files.xlsx"],
+        "csharp"
+    ),
 
-# Looking through C# files that DON'T have .designer in the extension
-def find_sql_data_sources(directory):
-    sql_data_sources = []
-    patterns = [r"SELECT ", r'SqlCommand\("', r'SqlDataAdapter\("']
+    (
+        os.path.abspath("../python/String-Finder-1.0.0/Parts-X"),
+        ["IREM/Parts/.ascx", 
+        [r"<asp:SqlDataSource"],
+        "IREM-Parts-ascx-files.xlsx"],
+        "dotnet"
+    ),
+    ]
 
+def handle_os_walk(patterns, root, file, sql_to_excel):
+    filepath = os.path.join(root, file)
+    # If you don't set the encoding here, you will
+    # keep getting charmap codec can't decode
+    # byte XXXX errors
+    with open(filepath, encoding="utf8") as f:
+        content = f.readlines()
+        for line_number, line in enumerate(content, 1):
+                for pattern in patterns:
+                    if re.search(pattern, line):
+                        # Trying to exclude commented out lines (lines that start with /)
+                        match = re.match(r'^/', line.strip())
+                        if not match:
+                            sql_to_excel.append(
+                                (file, line_number, line.strip())
+                            )
+                            # Break to prevent duplicate entries if diff patterns observed on same line
+                            break
+    return sql_to_excel
+
+def save_to_wb(directory, excel_props, extension):
+    match extension: 
+        case "csharp":
+            sql_src = handle_csharp(directory, excel_props[1])
+        case "dotnet":
+            sql_src = handle_dot_net(directory, excel_props[1])
+    # Don't create excel sheet if no patterns match 
+    if sql_src is None:
+        raise Exception("Your excel sheet will be null. Terminating...")
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["Path: ", excel_props[0]])
+    ws.append(["Pattern: ", str(excel_props[1])])
+    ws.append([""])
+    ws.append(["File", "Line Number", "Select Command"])
+    for file, line_number, command in sql_src:
+        ws.append([file, line_number, command])
+    excel_file = excel_props[2]
+    wb.save(excel_file)
+    print(f"Excel file '{excel_file}' has been created.")
+    return
+
+def handle_csharp(directory, patterns):
+    sql_to_excel = []
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".cs") and not file.endswith(".designer.cs"):
-                filepath = os.path.join(root, file)
-                with open(filepath, "r") as f:
-                    content = f.readlines()
-                    for line_number, line in enumerate(content, 1):
-                        for pattern in patterns:
-                            if re.search(pattern, line):
-                                # Exclude lines that contain //, we don't need to include commented lines
-                                match = re.match(r'^/', line.strip())
-                                if not match:
-                                    sql_data_sources.append(
-                                        (file, line_number, line.strip())
-                                    )
+                handle_os_walk(patterns, root, file, sql_to_excel)      
+    return sql_to_excel
 
-    return sql_data_sources
-
-
-sql_data_sources = find_sql_data_sources(directory)
-
-# Convert to excel file etc
-wb = Workbook()
-ws = wb.active
-ws.append(["File", "Line Number", "Select Command"])
-
-for file, line_number, command in sql_data_sources:
-    ws.append([file, line_number, command])
-
-excel_file = "IREM/Parts/c#-files.xlsx"
-wb.save(excel_file)
-print(f"Excel file '{excel_file}' has been created.")
-
-
-#  Looking through ONLY .ascx files
-def find_sql_data_sources2(directory):
-    sql_data_sources2 = []
-    patterns = patterns = [r"<asp:SqlDataSource"]
-#   print(os.listdir(directory))
+def handle_dot_net(directory, patterns):
+    sql_to_excel = []
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".ascx"):
-                filepath = os.path.join(root, file)
-                with open(filepath, encoding="utf8") as f:
-                    content = f.readlines()
-                    for line_number, line in enumerate(content, 1):
-                        for pattern in patterns:
-                            if re.search(pattern, line):
-                                sql_data_sources2.append(
-                                    (file, line_number, line.strip())
-                                )
+                handle_os_walk(patterns, root, file, sql_to_excel)      
+    return sql_to_excel
 
-    return sql_data_sources2
-
-
-sql_data_sources2 = find_sql_data_sources2(directory)
-
-
-# Convert to excel file etc
-wb = Workbook()
-ws = wb.active
-ws.append(["File", "Line Number", "Select Command"])
-
-for file, line_number, command in sql_data_sources2:
-    ws.append([file, line_number, command])
-
-excel_file = "IREM/Parts/ascx-files.xlsx"
-wb.save(excel_file)
-print(f"Excel file '{excel_file}' has been created.")
+for entry in entries:
+    save_to_wb(*entry)
